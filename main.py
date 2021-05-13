468b23ad52d8492715f748be903fbba39aeb2f73
Martika Wiszniewska <m.wiszniewska@techocean.pl>
2021-05-13 23:55:20 +0200

    Update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import vidaug.augmentors as va
 from PIL import Image
 import numpy as np
+import random
 
 
 def video2frames(directory):
@@ -19,8 +20,9 @@ def video2frames(directory):
         except AttributeError:
             break
         else:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            frames.append(image)
+            image_rgb = image
+                #cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frames.append(image_rgb)
         if cv2.waitKey(10) == 27:  # exit if Escape is hit
             break
     data = np.array(frames)
@@ -62,31 +64,69 @@ def frames2video(aug, filename, aug_type='_horizontal_flip'):
     video = cv2.VideoWriter(dir_, 0, 20, (width, height))
 
     for image in aug:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB )
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB )
         video.write(image)
 
     video.release()
 
 def slight_colour_change(change, image_list):
     for image in image_list:
-        for pixel in image:
-            a, b, c = pixel
-            if a + change < 255 < a - change:
-                pixel[0] = a
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                a, b, c = image[i][j]
+                if a + change < 255 < a - change:
+                    image[i][j][0] = a
     return image_list
 
-def background_colour_change(colour, image_list):
-    for image in image_list:
-        #trans_mask = np.zeros(image.shape[0], image.shape[1])
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+def give_random_background(path = r'C:\Users\Martina\Desktop\baza_danych_inz\back_ground'):
+    dir_list = os.listdir(path)
+    rand = random.randint(0, len(dir_list) - 1)
+    iterator = 0
+    for filename in dir_list:
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            if rand == iterator:
+                back_img = cv2.imread(os.path.join(path, filename))
+                back_img = cv2.cvtColor(back_img, cv2.COLOR_BGR2BGRA)
+                return back_img
+            iterator = iterator +1
+        else:
+            continue
+    return None
+
+
+def background_colour_change(colour, image_list, image_mode = False):
+    for i in range(image_list.shape[0]):
+        hsv_image = cv2.cvtColor(image_list[i], cv2.COLOR_BGR2HSV)
+        alpha_image = cv2.cvtColor(image_list[i], cv2.COLOR_BGR2BGRA)
         trans_mask = cv2.inRange(hsv_image, (5, 105, 10), (14, 170, 250))
         #((5, 105, 10), (14, 170, 250))tło przy pani z krótkimi włosami i grzywką na bok i grubszej pani o jadnej karnacji
-        kernel = np.ones((7, 7), np.uint8)
+        kernel = np.ones((7,7), np.uint8)
         trans_mask = cv2.morphologyEx(trans_mask, cv2.MORPH_OPEN, kernel)
-        #trans_mask = np.array(trans_mask, dtype = bool)
-        kernel2 = np.ones((1, 1), np.uint8)
-        trans_mask = np.array(cv2.dilate(trans_mask, kernel2, iterations=1), dtype=bool)
-        image[trans_mask] = (255, 255, 255)
+
+        #kernel2 = np.ones((1, 1), np.uint8)
+        #trans_mask = np.array(cv2.erode(trans_mask, kernel2, iterations=1), dtype=bool)
+
+        trans_mask = np.array(trans_mask, dtype=bool)
+        if image_mode:
+            back_image = give_random_background()
+            image2 = cv2.resize(back_image, (alpha_image.shape[1],alpha_image.shape[0]))
+            trans_mask = np.logical_not(trans_mask)
+            image2[trans_mask] = (0, 0, 0, 0)
+        else:
+            image2 = np.zeros((alpha_image.shape), np.uint8)
+            image2[trans_mask] = (255,0,255,255)
+
+        cv2.imwrite('dupa.jpg', image2)
+        image2 = cv2.GaussianBlur(image2, (5, 5), 0)
+        image2 = cv2.blur(image2, (3,3))
+        print(image2.shape)
+
+        image_list[i] = cv2.cvtColor(cv2.addWeighted(alpha_image, 1, image2, 0.8, 0.0), cv2.COLOR_BGRA2BGR)
+
+
+        #trans_mask = np.array(trans_mask, dtype=bool)
+        #image[trans_mask] = (255, 255, 255)
+        #image = cv2.GaussianBlur(image[:,:,0], (1, 1), sigmaX=4, sigmaY=4, borderType=cv2.BORDER_DEFAULT)
         print(f"trans_mask pierwszego zdjęcia --> {trans_mask}")
 
         #test
@@ -99,9 +139,10 @@ def go_through(directory):
             data = video2frames(os.path.join(directory, filename))
             if data is None:
                 continue
-            data_aug = background_colour_change(30, data)
-            frames2video(data_aug, filename, 'background_colour_white01_zamykanie')
-            # video_aug.save("out.avi", save_all=True, append_images=video_aug[1:], duration=100, loop=0)
+            data_aug = background_colour_change(30, data) #zmiana koloru tła
+            #data_aug = slight_colour_change(70, data) #zmiana przestrzeni barw?
+            frames2video(data_aug, filename, 'purple_background')
+            #video_aug.save("out.avi", save_all=True, append_images=video_aug[1:], duration=100, loop=0)
         else:
             continue
 
